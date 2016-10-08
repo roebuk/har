dc950166ad15c056f25b63a2065c979ae22c1a09
Kristian Roebuck <roebuck86@gmail.com>
2016-10-08 21:10:39 +0100

    Initial commit

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express()
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+
+app.set('view engine', 'pug')
+app.set('port', 3000)
+app.set('address', '0.0.0.0')
+app.use('/static', express.static('static'))
+
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+io.on('connection', function (socket) {
+  socket.on('triggerSound', function (data) {
+    io.emit('playSound', data)
+  })
+})
+
+server.listen(app.get('port'), app.get('address'), () => {
+  console.log(`Listening on ${app.get('address')}:${app.get('port')}`)
+})
---
+++ static/app.js
@@ -0,0 +1,73 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+  // HOLY FUCK THIS SHITTY CODE. SHOOT ME.
+  var audios = []
+  var numOfAudios = 9
+
+  for (var i = 1; i <= numOfAudios; i++) {
+    audios.push(new Howl({
+      src: [`/static/audio/${i}.mp3`]
+    }))
+  }
+
+  const socket = io(`${location.protocol}//${location.hostname}:3000`, {
+    transports: ['websocket']
+  })
+
+  const $container = document.querySelector('.container')
+  $container.addEventListener('click', emitClick)
+  var counter = 1
+
+  socket.on('playSound', function (data) {
+    counter++
+    audios[data.sound].play()
+    createAndPlaceElement(data, counter)
+
+    const $currentEl = document.querySelector('.dot:last-child')
+    startAnimation($currentEl)
+    cleanUpFromDOM($currentEl)
+  })
+
+  function getRandomSound() {
+    return Math.floor(Math.random() * numOfAudios) + 1
+  }
+
+  function startAnimation($el) {
+    setTimeout(function() {
+      $el.classList.add('grow')
+    }, 0.2);
+  }
+
+  function cleanUpFromDOM($el) {
+    setTimeout(function() {
+      $container.removeChild($el);
+    }, 7500)
+  }
+
+  function emitClick(e) {
+    const windowWidth = window.innerWidth
+    const windowHeight = window.innerHeight
+    const x = e.offsetX
+    const y = e.offsetY
+
+    const data = {
+      x: parseInt(((x  / windowWidth) * 100).toFixed(2)),
+      y: parseInt(((y / windowHeight) * 100).toFixed(2)),
+      sound: getRandomSound()
+    }
+
+    socket.emit('triggerSound', data)
+  }
+
+  function createAndPlaceElement(data, counter) {
+    const el = document.createElement('div')
+    el.className = `dot`
+    el.style.top = `calc(${data.y}% - 5rem)`
+    el.style.left = `calc(${data.x}% - 5rem)`
+
+    $container.appendChild(el)
+    return el
+  }
+
+
+})
